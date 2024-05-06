956fe423471a0e8302d4965d0711ae704cae54e0
ufy <ufuomaitwa@gmail.com>
2024-05-06 19:15:57 +0100

    ....

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+
+const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
+})
+
+console.log(db)
+module.exports = db;
